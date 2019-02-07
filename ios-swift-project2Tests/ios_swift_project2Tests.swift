1ebe4864ec6cc29d696e7eed81141edf428f4bcd
Jaizon Fernando Lubaton <jahskee@yahoo.com>
2019-02-07 18:59:51 -0500

    Initial Commit

---
+++ ios-swift-project2Tests/ios_swift_project2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_swift_project2Tests.swift
+//  ios-swift-project2Tests
+//
+//  Created by Mac on 2/7/19.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import XCTest
+@testable import ios_swift_project2
+
+class ios_swift_project2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
